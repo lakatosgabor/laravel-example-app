5b1ed3f48be2fa04ddb0dd66c8771bf956fcb7f3
Lakatos Gábor <lakatos.gabor998@gmail.com>
2026-03-18 18:38:51 +0100

    faker issue

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,11 +22,10 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array{
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
